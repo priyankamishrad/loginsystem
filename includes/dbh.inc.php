45c72252bd44196fa7ec3e312562fc67e37ff3f9
Priyanka Mishra <pmishradube@gmail.com>
2018-08-23 11:16:42 -0500

    Create dbh.inc.php

---
+++ includes/dbh.inc.php
@@ -0,0 +1,10 @@
+<?php
+
+//4 variables for connection to database
+
+$dbservername = "";
+$dbusername = "";
+$dbpassword = "";
+$dbname = "";
+
+$conn = mysqli_connect($dbservername, $dbusername, $dbpassword, $dbname);
